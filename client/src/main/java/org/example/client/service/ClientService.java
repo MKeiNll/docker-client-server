86aa9d365fa5adbfb841602f8e9d1d566027b78f
MKeiNll <levgirich@gmail.com>
2020-11-25 21:30:28 +0200

    implement client logic

---
+++ client/src/main/java/org/example/client/service/ClientService.java
@@ -0,0 +1,40 @@
+package org.example.client.service;
+
+import net.minidev.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Random;
+
+@Service()
+public class ClientService {
+
+    private RestTemplate restTemplate;
+    private HttpHeaders headers;
+    private URI uri;
+    private Random random;
+
+    public ClientService() {
+        restTemplate = new RestTemplate();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        random = new Random();
+    }
+
+    // A periodical task to imitate gameplay via generating random balance changes
+    @Scheduled(fixedRate = 1000)
+    public void imitateGameplay() throws URISyntaxException {
+        JSONObject balanceChangeRequest = new JSONObject();
+        balanceChangeRequest.put("transactionId", random.nextLong());
+        balanceChangeRequest.put("username", "player" + random.nextInt(25));
+        balanceChangeRequest.put("balanceChange", random.nextInt(100000) + "." + random.nextInt(100));
+        HttpEntity<String> request = new HttpEntity<String>(balanceChangeRequest.toString(), headers);
+        restTemplate.postForObject(new URI("http://localhost:9090/".concat(random.nextBoolean() ? "addFunds" : "withdrawFunds")), request, String.class);
+    }
+}
