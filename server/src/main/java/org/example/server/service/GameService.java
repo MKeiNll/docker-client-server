55e0b7bdf668bed16bfa97fae5db7ef72978a4dc
MKeiNll <levgirich@gmail.com>
2020-11-25 22:24:00 +0200

    fix build errors

--- server/src/main/java/org/example/server/service/GameService.java
+++ server/src/main/java/org/example/server/service/GameService.java
@@ -10,6 +10,7 @@ import org.example.server.exception.PlayerBlacklistedException;
 import org.example.server.repository.PlayerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,7 @@ public class GameService {
 
     private Logger logger = LoggerFactory.getLogger(GameService.class);
 
+    @Autowired
     public GameService(PlayerRepository playerRepository, GameProperties gameProperties) {
         this.playerRepository = playerRepository;
         this.gameProperties = gameProperties;
